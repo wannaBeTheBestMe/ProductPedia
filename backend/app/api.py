37fb4464e0007f2087044d5271719d6e1a36d7b7
Agastya Makkar <agastya.makkar@gmail.com>
2022-02-27 14:31:03 +0530

    Added /price API endpoint for scraping Am

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
+from requests_html import AsyncHTMLSession
+import time
 
 app = FastAPI()
 
@@ -16,7 +19,26 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# @app.get("/", tags=["root"])
-# async def read_root() -> dict:
-#     return {"message": "Welcome to this list"}
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to this list"}
 
+@app.get("/price/{url:path}", tags=["product"])
+async def get_price(url: str) -> dict:
+    beginning = time.time()
+
+    session = AsyncHTMLSession()
+    request = await session.get(url)
+    await request.html.arender()
+
+    product = {}
+    product["title"] = request.html.xpath('//*[@id="productTitle"]', first=True).text
+    try:
+        product["price"] = request.html.xpath('//*[@id="corePrice_desktop"]/div/table/tbody/tr[2]/td[2]/span[1]/span[2]', first=True).text
+    except:
+        product["price"] = request.html.xpath('//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[2]', first=True).text
+
+    end = time.time()
+    product["time"] = str(end - beginning)
+
+    return {"data": product}

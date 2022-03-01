ff79ecf1d82045c965a3d729cd60b2c0cbd1be98
Agastya Makkar <agastya.makkar@gmail.com>
2022-03-01 19:37:30 +0530

    Added recommendations component

---
+++ frontend/src/Recommendations/Recommendations.js
@@ -0,0 +1,15 @@
+import "./Recommendations.css";
+
+const Recommendations = () => {
+  return (
+    <section className="section-recommendations">
+      <h2 className="heading-recommendations">RECOMMENDATIONS</h2>
+      <p>
+        We thought long and hard about what you want in your product... and here
+        are products we think you’ll love!
+      </p>
+    </section>
+  );
+};
+
+export default Recommendations;

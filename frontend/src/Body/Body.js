aa96e05cf70a90c472e170515be7ecf9f4314f22
Agastya Makkar <agastya.makkar@gmail.com>
2022-03-01 22:43:56 +0530

    In the process of adding Formik FieldArray to Picks component, other minor refactoring

--- frontend/src/Body/Body.js
+++ frontend/src/Body/Body.js
@@ -1,12 +1,13 @@
 import "./Body.css";
 import Picks from "../Picks/Picks.js";
 import Filters from "../Filters/Filters.js";
+import Recommendations from "../Recommendations/Recommendations.js";
 
 const Body = () => {
   return (
     <div className="grid-container">
       <Picks />
-      s
+      <Recommendations/>
       <Filters />
     </div>
   );
--- frontend/src/Picks/Picks.js
+++ frontend/src/Picks/Picks.js
@@ -1,7 +1,104 @@
 import React from "react";
 import "./Picks.css";
-import { Formik, Field, Form, ErrorMessage, useField } from "formik";
+import {
+  Formik,
+  Field,
+  FieldArray,
+  Form,
+  ErrorMessage,
+  useField,
+} from "formik";
 import * as Yup from "yup";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="placeholder-indent url-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+const initialValues = {
+  urls: [""],
+};
+
+const validationSchema = Yup.object({
+  urls: Yup.string().url(),
+});
+
+const onSubmit = (values) => {
+  setTimeout(() => {
+    alert(JSON.stringify(values, null, 2));
+  }, 500);
+};
+
+const AddProductUrls = () => (
+  <div>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ values }) => (
+        <Form>
+          <FieldArray
+            name="urls"
+            render={(arrayHelpers) => (
+              <div>
+                {values.urls && values.urls.length > 0 ? (
+                  values.urls.map((friend, index) => (
+                    <div key={index}>
+                      <MyTextInput
+                        label=""
+                        name="url"
+                        type="text"
+                        placeholder="https://..."
+                      />
+                      <div className="add-remove-buttons">
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.insert(index, "")} // Insert an empty string at a position
+                          className="picks-buttons"
+                        >
+                          +
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => arrayHelpers.remove(index)} // Remove a URL from the list
+                          className="picks-buttons"
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => arrayHelpers.push("")}
+                    className="recommend-add-button picks-buttons"
+                  >
+                    {/* Message when user has removed all URLs from the list */}
+                    Add a URL
+                  </button>
+                )}
+                <div>
+                  <button
+                    type="submit"
+                    className="recommend-add-button picks-buttons"
+                  >
+                    Recommend!
+                  </button>
+                </div>
+              </div>
+            )}
+          />
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
 const Picks = () => {
   return (
@@ -11,14 +108,16 @@ const Picks = () => {
         Thought of some products you might want to buy? Paste their URLs below!
       </p>
 
-      <label htmlFor="url"></label>
-      <input
-        type="text"
-        id="url"
-        className="placeholder-indent url-input"
-        name="url"
-        placeholder="https://..."
-      />
+      <AddProductUrls />
+
+      {/* <label htmlFor="url"></label> */}
+      {/* <input */}
+      {/*   type="text" */}
+      {/*   id="url" */}
+      {/*   className="placeholder-indent url-input" */}
+      {/*   name="url" */}
+      {/*   placeholder="https://..." */}
+      {/* /> */}
     </section>
   );
 };
